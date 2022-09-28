b9bc89cfbcd4871f72978efa8a21a82da36262e6
Asif Iqbal <iqbal1@ualberta.ca>
2022-09-28 03:36:38 -0600

    fixd newsfeed

--- src/pages/NewsFeed/NewsFeed.js
+++ src/pages/NewsFeed/NewsFeed.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../../components/Header/Header";
 import "./NewsFeed.scss";
+import ProfilePic from '../../assets/images/feelswowman.png';
 
 const NewsFeed = () => {
   return (
@@ -8,13 +9,19 @@ const NewsFeed = () => {
       <Header />
       <div className="newsfeed-card">
         <div className="newsfeed-card-single">
+          <div className="newsfeed-card-top">
+        <img className="tablet-plus-header__profile" src={ProfilePic} alt="profilepic"></img>
           <h1 className="newsfeed-card-header">User</h1>
+          </div>
           <video id="videoPlayer" width="650" controls muted="muted">
             <source src="http://localhost:5000/video/moo" type="video/mp4" />
           </video>
         </div>
         <div className="newsfeed-card-single">
+          <div className="newsfeed-card-top">
+        <img className="tablet-plus-header__profile" src={ProfilePic} alt="profilepic"></img>
           <h1 className="newsfeed-card-header">User</h1>
+          </div>
           <video id="videoPlayer" width="650" controls muted="muted">
             <source src="http://localhost:5000/video/int" type="video/mp4" />
           </video>
